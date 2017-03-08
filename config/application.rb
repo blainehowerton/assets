e38ef2eee2936ced07e758970ed13d29535b84a3
Blaine Howerton <bhowerton@yghdtv.com>
2017-03-08 00:25:48 -0700

    converted time zone from UTC to current zone.

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,6 @@ module Assets
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+    config.time_zone = Time.now.zone
   end
 end
